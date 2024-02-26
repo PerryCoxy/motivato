9ce42022072d8fa4bf9f7f05240b4b1bd68cc15a
wja36rus <48429607+wja36rus@users.noreply.github.com>
2024-02-26 22:34:45 +0300

    add draggable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import { Heading } from "@/components/ui/Heading";
-import UserCard from "@/components/ui/UserCard";
+import Draggable from '@/components/ui/Draggable'
+import { Heading } from '@/components/ui/Heading'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Heading title='Здесь пока ничего нет' />
-      <div className="users">
-        <UserCard name={"user 1"}/>
-        <UserCard name={"user 2"}/>
-        <UserCard name={"user 3"}/>
-        <UserCard name={"user 4"}/>
-        <UserCard name={"user 5"}/>
-      </div>
-      <div className="result-users">
-
-      </div>
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			<Heading title='Здесь пока ничего нет' />
+			<Draggable />
+		</main>
+	)
 }
